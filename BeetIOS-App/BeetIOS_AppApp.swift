01871d453c67d2caa450e9fffb68bc944a38c01e
Yashwanth Bharadwaj <86095452+Yash-bharadwaj@users.noreply.github.com>
2025-12-01 01:27:05 +0530

    Initial Commit

---
+++ BeetIOS-App/BeetIOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeetIOS_AppApp.swift
+//  BeetIOS-App
+//
+//  Created by Yashwanth Bharadwaj on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BeetIOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeetIOS-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeetIOS-App
+//
+//  Created by Yashwanth Bharadwaj on 01/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
